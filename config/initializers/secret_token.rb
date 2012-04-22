a50ba6bb377325204e151d04bdef6ae88eee895f
Patrick Lee <patrick.robin.lee@gmail.com>
2012-04-22 17:47:40 -0400

    First commit with framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mijikai::Application.config.secret_token = '22074c5914202abee6f632038a34320ce24e004f6be8a836f4476a01b1f4414836db292966663ca5f6f4b20eec4da78e9ae4ec97e0a9946122b03be121399fc2'
