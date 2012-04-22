1395166151697ad507cdd704f15753a68f21919b
Patrick Lee <patrick.robin.lee@gmail.com>
2012-04-22 18:25:32 -0400

    Added devise gem to configuration.

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'terms'" do
+    it "returns http success" do
+      get 'terms'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'privacy'" do
+    it "returns http success" do
+      get 'privacy'
+      response.should be_success
+    end
+  end
+
+end
