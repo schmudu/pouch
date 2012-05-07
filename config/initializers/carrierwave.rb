eb0ffe7fe79cb3169f9764a5ea7bf6a311f671be
Patrick Lee <patrick.robin.lee@gmail.com>
2012-05-07 18:45:59 -0400

    Fixed some setting and links on the home page for deployment.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,8 @@ if Rails.env.production?
       :aws_secret_access_key  => 'O6YIJQqzR2PfLrLAAoC49uO52cH+AYuH2jA15f8j',       # required
       :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = 'lessonpouch'                               # required
+    #config.fog_directory  = 'lessonpouch'                          # required, production setting
+    config.fog_directory  = 'testpouch'                             # required, test setting
     #config.fog_host      = 'https://assets.example.com'            # optional, defaults to nil
     config.fog_public     = false                                   # optional, defaults to true
     config.storage        = :fog
