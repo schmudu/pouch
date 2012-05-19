ef5210e523f57ab4b6e2aeca7a5fb5adc5ef3bd4
Patrick Lee <patrick.robin.lee@gmail.com>
2012-05-18 23:46:19 -0400

    Tried to add a change to fix heroku deployment.

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,4 @@
+=begin
 if ENV['BONSAI_INDEX_URL']
   Tire.configure do
     url "http://index.bonsai.io"
@@ -7,4 +8,14 @@ else
   app_name = Rails.application.class.parent_name.underscore.dasherize
   app_env = Rails.env
   BONSAI_INDEX_NAME = "#{app_name}-#{app_env}"
+end
+=end
+if ENV['BONSAI_INDEX_URL']
+  bonsai_uri = URI.parse(ENV['BONSAI_INDEX_URL'])
+  Tire.configure do
+    url "http://index.bonsai.io"
+  end
+  BONSAI_INDEX_NAME = bonsai_uri.path[1..-1]
+else
+  BONSAI_INDEX_NAME = "resources"
 end
