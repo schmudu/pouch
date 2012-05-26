cfdc656cbedaaf4c3a78caa066a0cf5b4f810977
Patrick Lee <patrick.robin.lee@gmail.com>
2012-05-26 17:24:36 -0400

    Fixed favorites and added random quotes.

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,2 +1,7 @@
 module FavoritesHelper
+  def user_already_has_favorite?
+    favorite = Favorite.find(:first, :conditions => ["user_id = ? and resource_id =?", current_user.id, params[:id]])
+    return false if favorite.nil?
+    return true
+  end
 end
